089561d1f70b9ddbc3a64e34ea46df517ea84ea7
quawer <info@quawer.com>
2015-10-17 10:36:38 +0200

    registration and payment update

--- sub/manager/php/registrate.php
+++ sub/manager/php/registrate.php
@@ -1,18 +1,12 @@
 <?php
-include (httpful.phar);
+include ('httpful.phar');
 if (isset($_POST["submit"])) {
-    $venue_name = $_POST['venue_email'];
     $venue_name = $_POST['venue_name'];
+    $venue_email = $_POST['email'];
     $password = $_POST['password'];
-    $password_confirmation = $_POST['password_confirmation'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $business_name = $_POST['business_name'];
-    $adressFirst = $_POST['adressFirst'];
-    $adreseSecond = $_POST['adressSecond'];
-    $city = $_POST['city'];
-    $adrese = $_POST['adrese'];
     $state = $_POST['state'];
+    $city = $_POST['city'];
+    $adressFirst = $_POST['address'];
     
 $url = "https://api.bandcloud.net/agents/register";
 $body = 'venue_email=' . $body = 'venue_name=' . $venue_name . '&password=' . $password . '&password_confirmation' . $password . '&first_name' . $first_name . '&last_name' . $secondname . '&business_name' . $business_name . '&adressFirst' . $adressFirst . '&adressSecond' . $adressSecond . '&city' . $city . '&adrese' . $adrese . '&state' . $state .;
@@ -25,22 +19,5 @@ $body = 'venue_email=' . $body = 'venue_name=' . $venue_name . '&password=' . $p
 			return $response->body->accountId;
 		else
 			return $response->body->status;
-	}   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
-
-
+	}
+?>
